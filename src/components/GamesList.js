f1eb117382bbc1b82f7c6d99849f282db1995964
Elton Jothi <elton.jj@gmail.com>
2018-05-12 19:55:10 +0530

    Fixed URL

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -6,13 +6,13 @@ class GamesList extends React.Component {
       <ul className="list-group">
       {
         this.props.list.map(function(data) {
-          return <li className="list-group-item" data-category={data.title} >
+          return <a href={"http://www.ign.com" + data.url} class="list-group-item list-group-item-action" target="_blank">
           {data.editors_choice === "Y" &&
           <span class="badge">Editors choice</span>
           }
           <h3 class="list-group-item-heading">{data.title}</h3>
           <h4 class="list-group-item-heading">{data.platform}</h4>
-          <p>Genre : {data.genre} | Score : {data.score} | Released in {data.release_year}</p></li>
+          <p>Genre : {data.genre} | Score : {data.score} | Released in {data.release_year}</p></a>
         })
        }
       </ul>
